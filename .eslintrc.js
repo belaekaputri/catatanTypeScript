9eeb32c8ae13e9af2fadb73add01b01c1477058b
belaekaputri <belaekaputri28@gmail.com>
2023-05-11 17:14:57 +0700

    start project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.export = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: [
+             "@typescript-eslint"
+             ],
+    extends: [
+              "eslint:recommended",
+              "plugin:@typescript-eslint/recommended"
+             ],
+ }
